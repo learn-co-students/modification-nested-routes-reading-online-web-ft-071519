c80927302901d1a7a488e9a3022a99862b1cc114
BaronLip <baronlip@gmail.com>
2019-09-24 16:30:56 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,58 @@
 class PostsController < ApplicationController
 
-  def index
-    if params[:author_id]
-      @posts = Author.find(params[:author_id]).posts
-    else
-      @posts = Post.all
+    def index
+        if params[:author_id]
+            @posts = Author.find(params[:author_id]).posts
+        else
+            @posts = Post.all
+        end
     end
-  end
 
-  def show
-    if params[:author_id]
-      @post = Author.find(params[:author_id]).posts.find(params[:id])
-    else
-      @post = Post.find(params[:id])
+    def show
+        if params[:author_id]
+        @post = Author.find(params[:author_id]).posts.find(params[:id])
+        else
+        @post = Post.find(params[:id])
+        end
     end
-  end
 
-  def new
-    @post = Post.new
-  end
+    def new
+        # @post = Post.new
+        @post = Post.new(author_id: params[:aurthor_id])
+    end
 
-  def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to post_path(@post)
-  end
+    def create
+        if params[:author_id] && !Author.exists?(params[:author_id])
+            redirect_to authors_path, alert: "Author not found."
+        else
+            @post = Post.new(author_id: params[:author_id])
+        end
+    end
 
-  def update
-    @post = Post.find(params[:id])
-    @post.update(params.require(:post))
-    redirect_to post_path(@post)
-  end
+    def edit
+        if params[:author_id]
+            author = Author.find_by(id: params[:author_id])
+            if author.nil?
+                redirect_to authors_path, alert: "Author not found."
+            else
+                @post = author.posts.find_by(id: params[:id])
+                redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+            end
+        else
+            @post = Post.find(params[:id])
+        end
+    end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+    def update
+        @post = Post.find(params[:id])
+        # @post = Author.find(params[:author_id]).posts.find(params[:id])
+        @post.update(params.require(:post))
+        redirect_to post_path(@post)
+    end
 
-  private
+    private
 
-  def post_params
-    params.require(:post).permit(:title, :description)
-  end
+    def post_params
+        params.require(:post).permit(:title, :description, :author_id)
+    end
 end
